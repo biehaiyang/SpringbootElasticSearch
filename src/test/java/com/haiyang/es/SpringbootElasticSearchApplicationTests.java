d449ed45103e16c81baa5abe07dbe85ac7039da9
haiyang’ <48912814+biehaiyang@users.noreply.github.com>
2020-05-20 16:45:15 +0800

    Add files via upload

---
+++ src/test/java/com/haiyang/es/SpringbootElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiyang.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
